018afd5812f10c172f1d2276a58e85d88609f80f
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-05 23:24:00 +0200

    Create new project pixel-positions
    Install and configure tailwindcss for development
    add layout file and change welcome file to fit project needs

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <section>
+        <h1>Top Jobs</h1>
+
+        <div class="p-4 bg-white/5">
+            <div>
+                <div>Laracasts</div>
+                <div>
+                        <h3>Video Producer</h3>
+                        <p>Full Time - From $60,000</p>
+                </div>
+
+                <div>
+                    <div>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                    </div>
+
+                    <img src="https://placehold.co/42" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+</x-layout>
