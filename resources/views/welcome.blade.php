93b05a489b0822a48179721d89275744f48801b3
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-06 15:07:33 +0200

    Add blade components for different sections
    Breakdown the welcome and layout page into different component files
    This adds simplicity and ensures easier fixes in future updates
    Job-card, section-heading and tag files components added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,35 @@
 <x-layout>
-    <section>
-        <h1>Top Jobs</h1>
-
-        <div class="p-4 bg-white/5">
-            <div>
-                <div>Laracasts</div>
-                <div>
-                        <h3>Video Producer</h3>
-                        <p>Full Time - From $60,000</p>
-                </div>
-
-                <div>
-                    <div>
-                        <a href="#"></a>
-                        <a href="#"></a>
-                        <a href="#"></a>
-                    </div>
-
-                    <img src="https://placehold.co/42" alt="">
-                </div>
+    <div class="space-y-10">
+        <section>
+            <x-section-heading>Featured Jobs</x-section-heading>
+    
+            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                <x-job-card />
+                <x-job-card />
+                <x-job-card />
             </div>
-        </div>
+         </section>
+    
+         <section>
+            <x-section-heading>Tags</x-section-heading>
 
-    </section>
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+    
+         </section>
+    
+         <section>
+            <x-section-heading>Recent Jobs</x-section-heading>  
+         </section>
+    </div>
 </x-layout>
