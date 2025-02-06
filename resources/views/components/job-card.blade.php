078259fbe1364182ed8ca3631923a0b08123cfda
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-07 01:28:49 +0200

    Extract custom hover styling into panel component
    To reduce duplication in hover styling extract duplicated styles into
    separate component that can be referenced when needed

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
+<x-panel class="flex flex-col text-center">>
     <div class="self-start text-sm">Laracasts</div>
     <div class="py-8">
             <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
@@ -14,4 +14,4 @@
 
         <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
