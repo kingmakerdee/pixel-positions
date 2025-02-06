c7a17a7b4a65f18b4ddeb4e19c5e8d37dcfd63b3
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-07 01:03:26 +0200

    Clean up blade components and improve appeal
    Add custom fontSize in tailwind.config to make tag font smaller
    add hover styling and color transitions to job-card and job-card-wide

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ export default {
             fontFamily: {
                 "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
             },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            },
         },
     },
     plugins: [],
