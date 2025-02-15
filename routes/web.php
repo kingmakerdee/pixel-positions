5fbd294a98f7ed0c5098b96de509d48a1522dd0a
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-15 21:52:22 +0200

    Add auth process to project
    Add routes for login, logout and registration
    Create controllers for routes
    Add view for registration and forms to be used
    Add blade components for use in auth process

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\registeredUserController;
+use App\Http\Controllers\sessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [registeredUserController::class, 'create']);
+Route::post('/register', [registeredUserController::class, 'store']);
+
+Route::get('/login', [sessionController::class, 'create']);
+Route::post('/login', [sessionController::class, 'store']);
+Route::post('/logout', [sessionController::class, 'destroy']);
