475511b185945a9d6de637d6c7b10cbd4337c987
Kingmakerdee <denzelndlovu63@gmail.com>
2025-02-16 01:59:17 +0200

    Seperate user view and guest view in layout
    edit guest route middleware wrapper to correct spelling of middleware

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
-route::midlleware('guest')->group(function () {
+route::middleware('guest')->group(function () {
     Route::get('/register', [registeredUserController::class, 'create']);
     Route::post('/register', [registeredUserController::class, 'store']);
-    
+
     Route::get('/login', [sessionController::class, 'create']);
     Route::post('/login', [sessionController::class, 'store']);
 });
